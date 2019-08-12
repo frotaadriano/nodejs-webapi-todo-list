9a532f92716370f0e57b54622196a6ee97e64f78
Adriano Frota <adriano.frota.silva@avanade.com>
2019-08-11 22:17:41 -0300

    Config Heroku 2 porta

--- src/config/server.js
+++ src/config/server.js
@@ -1,4 +1,9 @@
-const port = 3003
+const port = process.env.PORT || 3000
+
+// app.listen(process.env.PORT || 3000, function(){
+//     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+//   });
+  
 
 const bodyParser = require('body-parser')
 const express = require('express')
