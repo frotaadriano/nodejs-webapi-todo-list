6dde41526110165d14ec8f41b4cddaa8dfd8c860
Adriano Frota <adriano.frota.silva@avanade.com>
2019-08-11 22:03:47 -0300

    Configuracao para deploy no Heroku

--- src/config/database.js
+++ src/config/database.js
@@ -1,3 +1,8 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
-module.exports = mongoose.connect('mongodb://localhost/afstodo',{ useNewUrlParser: true })
+
+const url = process.env.MONGOLAB_URL ? process.env.MONGOLAB_URL : 'mongodb://localhost/afstodo'
+
+module.exports = mongoose.connect(url,{ useNewUrlParser: true })
+
+//mongoose.Error.messages.general.required = "xpto 1"
